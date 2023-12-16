e94c021c3bdd374090076b415ba5a05398d64e35
HonyaDew <Trolofoks@gmail.com>
2023-12-16 18:07:56 +0700

    UploadController is a little refactored.

--- src/main/kotlin/com/honya/feature/upload/UploadController.kt
+++ src/main/kotlin/com/honya/feature/upload/UploadController.kt
@@ -2,6 +2,7 @@ package com.honya.feature.upload
 
 import com.honya.database.photo.Photos
 import com.honya.database.product.ProductDTO
+import com.honya.database.product.ProductType
 import com.honya.database.product.Products
 import io.ktor.http.*
 import io.ktor.server.application.*
@@ -12,42 +13,63 @@ import java.util.*
 
 class UploadController() {
     suspend fun performUpload(call: ApplicationCall) {
-        val receive = call.receive<UploadReceiveRemote>()
+        try {
+            val receive = call.receive<UploadReceiveRemote>()
 
-        //TODO create constant file with all password & no upload to github
-        if (receive.adminPassword == "123") {
-            val productId = UUID.randomUUID().toString().substring(0, 20)
+            if (authenticateAdmin(receive.adminPassword)) {
+                val productDTO = createProductDTO(receive)
 
-            val photoIds = if (receive.photoFile != null) {
-                val byteArrayPhotos = receive.photoFile.map { base64String ->
-                    Base64.getDecoder().decode(base64String.removePrefix("data:image/png;base64,"))
-                }
+                val productType = ProductType.fromKey(receive.type) ?: ProductType.OTHER
 
-                byteArrayPhotos.map { byteArray ->
-                    Photos.insert(byteArray)
+                val insert = ProductDTO(
+                    productId = productDTO.productId,
+                    name = productDTO.name,
+                    type = productType.key,
+                    photoIds = productDTO.photoIds,
+                    description = productDTO.description
+                )
+
+                Products.insert(insert)
+
+                if (Products.getProductInfo(productDTO.productId)?.name == productDTO.name) {
+                    call.respond(UploadResponseRemote(productId = productDTO.productId))
+                } else {
+                    call.respond(HttpStatusCode.InternalServerError, "Unexpected error")
                 }
             } else {
-                null
+                call.respond(HttpStatusCode.BadRequest, "Wrong admin password")
+            }
+        } catch (e: Exception) {
+            call.respond(HttpStatusCode.InternalServerError, "An error occurred: ${e.message}")
+        }
+    }
+
+    private fun authenticateAdmin(adminPassword: String): Boolean {
+        //TODO better secure secure authentication system like OAuth or JWT
+        return adminPassword == "123"
+    }
+
+    private fun createProductDTO(receive: UploadReceiveRemote): ProductDTO {
+        val productId = UUID.randomUUID().toString().substring(0..10)
+
+        val photoIds = if (receive.photoFile != null) {
+            val byteArrayPhotos = receive.photoFile.map { base64String ->
+                Base64.getDecoder().decode(base64String.removePrefix("data:image/png;base64,"))
             }
 
-            val productDTO = ProductDTO(
-                productId = productId,
-                name = receive.name,
-                type = receive.type,
-                photoIds = photoIds,
-                description = receive.description
-            )
-
-            Products.insert(productDTO)
-
-
-            if (Products.getProductInfo(productId)?.name == productDTO.name) {
-                call.respond(UploadResponseRemote(productId = productId))
-            } else {
-                call.respond(HttpStatusCode.InternalServerError, "Unexpected error")
+            byteArrayPhotos.map { byteArray ->
+                Photos.insert(byteArray)
             }
         } else {
-            call.respond(HttpStatusCode.BadRequest, "Wrong admin password")
+            null
         }
+
+        return ProductDTO(
+            productId = productId,
+            name = receive.name,
+            type = receive.type,
+            photoIds = photoIds,
+            description = receive.description
+        )
     }
 }
