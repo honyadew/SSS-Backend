348c496ebbb7c861b5d914710ff50ead476a02ad
HonyaDew <Trolofoks@gmail.com>
2023-12-16 19:35:16 +0700

    EmailValidator now is doing something.

--- src/main/kotlin/com/honya/utils/EmailValidator.kt
+++ src/main/kotlin/com/honya/utils/EmailValidator.kt
@@ -1,3 +1,22 @@
 package com.honya.utils
 
-fun String.isValidEmail() : Boolean = true
+fun String.isValidEmail() : Boolean {
+    val containsAtSymbol = this.count { it == '@' } == 1
+
+    if (containsAtSymbol){
+        val split = this.split('@')
+
+        val containsDot = split[1].contains(".") && split[1].indexOf(".") == split[1].lastIndexOf(".")
+
+        if (containsDot){
+
+            val splitParts = listOf(split[0]) + split[1].split('.')
+
+            val isCorrect = splitParts.all { it.length in (1..30) }
+
+            val isValidLocalPart = split[0].matches("[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+".toRegex())
+
+            return isCorrect && isValidLocalPart
+        } else {return false}
+    } else {return false}
+}
